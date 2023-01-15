fac5e97ba81296bb6a88fe7b9f7d9ccc1883b2a2
Danish <dan71ish@gmail.com>
2023-01-15 12:57:39 +0530

    more questino added

--- 12 - Quiz App/app.js
+++ 12 - Quiz App/app.js
@@ -1,99 +1,138 @@
-const quizData = [
+const questionData = [
     {
-        question: "Who is the founder of Facebook",
-        a:'Elon Musk',
-        b:'Peter Thiel',
+        question:"Who is the CEO of Facebook",
+        a:'Elon musk',
+        b:'Jeff Bezos',
         c:'Mark Zuckerberg',
-        d:'Steve Jobs',
-        correct:'c' 
-    } ,
-    {
-        question:"How much it cost elon to buy twitter",
-        a:'44$ Billion',
-        b:'4$ Billion',
-        c:'23$ Billion',
-        d:'80 Hazaar',
-        correct:'a'
+        d:'Jack dorsey',
+        correct:'c'
     },
     {
-        question:"What is most popular song in youtube",
-        a:'Dusk till Down',
-        b:'Shape of you',
-        c:'Selfie maine le li aaj',
-        d:'Despacito',
-        correct:'d'
-    },
-    {
-        question:"Who saved apple to go bankrupt",
-        a:'Xerox',
-        b:'Microsoft',
-        c:'Nintendo',
-        d:'Samsung',
+        question:"In which year india got independence",
+        a:'1999',
+        b:'1947',
+        c:'1964',
+        d:'1960',
         correct:'b'
     },
     {
-        question:"What is the full form of 'DevOps' ",
-        a:'Development &opportunity',
-        b:'Development &OpenSource',
-        c:'Development & Operation',
-        d:'Development & Orange',
+        question:"Which language computer understand without translation",
+        a:'Binary bit (0/1)',
+        b:'Javascript',
+        c:'Assembly',
+        d:'Python',
+        correct:'a'
+    },
+    {
+        question:"Who is new owner of Twitter",
+        a:'Jeff Bezos',
+        b:'Mark Zuckerberg',
+        c:'Elon musk',
+        d:'Jack dorsey',
         correct:'c'
+    },
+    {
+        question:"How much Facebook paid to buy Whatsapp",
+        a:'19 Billion',
+        b:'10 Billion',
+        c:'1 Billion',
+        d:'4 Million',
+        correct:'a'
+    },
+    {
+        question:"How is the current CEO of Apple",
+        a:'Mike tyson',
+        b:'Jeff Bezos',
+        c:'Gautam Adani',
+        d:'Tim cook',
+        correct:'d'
+    },
+    {
+        question:"Who is the most popular star in india",
+        a:'Salman Khan',
+        b:'Ajay Devgan',
+        c:'Shahrukh Khan',
+        d:'Saif ali Khan',
+        correct:'c'
+    },
+    {
+        question:"What is the name of SRK house",
+        a:'Mannat',
+        b:'Antillia',
+        c:'Jalsa',
+        d:'Heaven',
+        correct:'a'
+    },
+    {
+        question:"In which age a baby should go to school",
+        a:'5yrs',
+        b:'10yrs',
+        c:'3yrs',
+        d:'12yrs',
+        correct:'c'
+    },
+    {
+        question:"Which thing a human should give more importance",
+        a:'Health',
+        b:'Wealth',
+        c:'Women',
+        d:'Cloth',
+        correct:'a'
     }
-];
-let quizz = document.getElementById('quiz');
-const answersEl = document.querySelectorAll('.answer');
-const questionEl = document.getElementById('question');
-const submitBtn = document.getElementById('submit');
-const a_text = document.getElementById('a_text');
-const b_text = document.getElementById('b_text');
-const c_text = document.getElementById('c_text');
-const d_text = document.getElementById('d_text');
+]
+let questionEl = document.getElementById('question');
+let answerA = document.getElementById('input-a');
+let answerB = document.getElementById('input-b');
+let answerC = document.getElementById('input-c');
+let answerD = document.getElementById('input-d');
+const btn = document.getElementById('btn');
+let radio = document.querySelectorAll('input[type="radio"]');
+let quizEl = document.getElementById('quiz-ul')
+
+
 let currentQuiz = 0;
 let score = 0;
-loadQuiz();
+deSelect();
 
-function loadQuiz(){
-    deSelectEl();
-    const currentQuizData = quizData[currentQuiz];
-    questionEl.innerHTML = currentQuizData.question;
-    a_text.innerText = currentQuizData.a;
-    b_text.innerText = currentQuizData.b;
-    c_text.innerText = currentQuizData.c;
-    d_text.innerText = currentQuizData.d;
+function load(){
+    questionEl.innerText = questionData[currentQuiz].question;
+    answerA.innerText = questionData[currentQuiz].a;
+    answerB.innerText = questionData[currentQuiz].b;
+    answerC.innerText = questionData[currentQuiz].c;
+    answerD.innerText = questionData[currentQuiz].d;
 }
-
-function getSelected(){
+btn.addEventListener('click',()=> {
+    let answer = Select();
+    if(answer){
+        if(answer === questionData[currentQuiz].correct){
+            score++;
+            console.log(score);
+        }
+        currentQuiz++;
+    if(currentQuiz < questionData.length){
+        deSelect();
+    } 
+    else{
+        questionEl.innerText = "";
+        quizEl.innerHTML = `
+                <h1>You answered correctly at ${score}/${questionData.length} questions.</h1>
+                
+                <button onclick="location.reload()">Reload</button>`
+    }
+}
+});
+function deSelect(){
+    radio.forEach(number => {
+        number.checked = false;
+    });
+    load();
+}
+function Select(){
     let answer = undefined;
-
-    answersEl.forEach((ans) => {
-        if(ans.checked){
-         answer = ans.id;   
+    radio.forEach(number => {
+        if(number.checked){
+            answer = number.id;
         }
     });
     return answer;
 }
-
-
-function deSelectEl(){
-    answersEl.forEach((ans) => {
-     ans.checked = false;
-    });
-}
-
-submitBtn.addEventListener('click',() => {
-   const answer = getSelected();
-   console.log(score);
-    if (answer) {
-        if (answer === quizData[currentQuiz].correct) {
-            score++;
-        }
-        currentQuiz++;
-    if(currentQuiz < quizData.length){
-        loadQuiz();
-    }
-    else{
-      quizz.style.padding = '2em'
-      quizz.innerHTML = `<h2>You answered correctly at ${score} / ${quizData.length} question.</h2 >`
-    }
-   }
-});
